6d6e6a54887bf0b0bf51d4e32df1b6ec3512018c
Maxim Gordienok <aikismax@gmail.com>
2011-10-11 15:01:58 +0300

    add 'devise to gemfile'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testapp::Application.config.secret_token = 'a2b45ffe4ec1512c2c3eab13ede4a72683d74fdb9026687850d334813fb9ecb52eb181c318add11ee368c8fc752df4b1eb7af97612e75a2902c4968ce9e1403d'
